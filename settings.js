ee8c4d4aea049417b7f384cb66b13ab1a374b278
GitaVictoria <gitanuraeniid@gmail.com>
2025-01-12 14:17:58 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -5,13 +5,13 @@ Replace Your Key with the apikey that you get in your profile.
 */
 
 module.exports = {
-  ownerUsernames: [""],//Your Username
-  ownerNumber: "",//Your Number 
-  telegramBotToken: "",//Your Bot Tokens
+  ownerUsernames: ["http://t.me/USER404nothuman"],//Your Username
+  ownerNumber: "6281286883165",//Your Number 
+  telegramBotToken: "7532366460:AAEkXsM8ZsAqqJaimpIK8p7lt0pzDk684f8",//Your Bot Tokens
   imageUrl: "https://btch.pages.dev/file/313139c543db723fb67e6.jpg",//Thumbnail Url
   buttonUrl: "https://github.com/ERLANRAHMAT/telebot",//Script Url
   api: "https://api.betabotz.eu.org",
-  apikey: "Your Key",//Change Your Apikey
+  apikey: "Btz-bUPYj",//Change Your Apikey
   mess: {
     eror: "Internal Server Eror 😵",
     owner: "Sorry, this command can only be accessed by the owner!",
