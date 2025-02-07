db14e932e6d286f32aadeec445692e7856fdedae
𝐄𝐦𝐩𝐢𝐫𝐞 𝐓𝐞𝐜𝐡 [ 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 ] <151479661+efeurhobobullish@users.noreply.github.com>
2025-02-07 15:54:10 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,13 +5,13 @@ Replace Your Key with the apikey that you get in your profile.
 */
 
 module.exports = {
-  ownerUsernames: [""],//Your Username
-  ownerNumber: "",//Your Number 
+  ownerUsernames: ["𝐸𝑚𝑝𝑖𝑟𝑒 𝑇𝑒𝑐ℎ"],//Your Username
+  ownerNumber: "2348078582627",//Your Number 
   telegramBotToken: "",//Your Bot Tokens
   imageUrl: "https://btch.pages.dev/file/313139c543db723fb67e6.jpg",//Thumbnail Url
   buttonUrl: "https://github.com/ERLANRAHMAT/telebot",//Script Url
   api: "https://api.betabotz.eu.org",
-  apikey: "Your Key",//Change Your Apikey
+  apikey: "Btz-5Dxu7",//Change Your Apikey
   mess: {
     eror: "Internal Server Eror 😵",
     owner: "Sorry, this command can only be accessed by the owner!",
